7ba9386a1d73132f91ac58926ee1e2f5defd9c6f
Arjunppp <arjunmohanak@gmail.com>
2024-07-22 10:23:03 +0530

    chat app personal message completed

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ import mongoose from 'mongoose';
 import { User } from './models/userModel.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import http from  'http';
+import http from 'http';
 import { Server } from 'socket.io';
 
 const port = 5000;
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server); //this will handle the socket request 
+const io = new Server(server); //this will handle the socket request
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
-
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.json());
@@ -25,15 +22,14 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/blogSite').then(() => {
     console.log('Db is connected');
-})
-
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'html', 'index.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'html', 'login.html'))
+    res.sendFile(path.resolve(__dirname, 'public', 'html', 'login.html'));
 });
 
 app.post('/login', async (req, res) => {
@@ -42,27 +38,21 @@ app.post('/login', async (req, res) => {
     let ispasswordMatch = await bcrypt.compare(password, result[0].password);
 
     if (ispasswordMatch) {
-        let tokenData = {"id":result[0]._id, "username":result[0].username ,"role":result[0].role};
-        const token = jwt.sign({tokenData}, 'CHATSECRETKEY');
+        let tokenData = { "id": result[0]._id, "username": result[0].username, "role": result[0].role };
+        const token = jwt.sign({ tokenData }, 'CHATSECRETKEY');
         res.cookie('userToken', token).redirect('/chat');
-    }
-    else {
+    } else {
         res.redirect('/login');
-
     }
-
-})
+});
 
 app.get('/signup', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'html', 'signup.html'))
-})
+    res.sendFile(path.resolve(__dirname, 'public', 'html', 'signup.html'));
+});
 
-app.get('/chat' , (req ,res) => 
-{
+app.get('/chat', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'html', 'chat.html'));
-})
-
-
+});
 
 const authenticate = (socket, next) => {
     const token = socket.handshake.auth.token;
@@ -80,15 +70,16 @@ const authenticate = (socket, next) => {
 const userSockets = new Map();
 
 io.use(authenticate).on('connection', (socket) => {
-    console.log('User connected:', socket.user);
+    console.log('User connected:', socket.id);
     userSockets.set(socket.user, socket);
-    console.log('Current userSockets:', userSockets);
+    console.log('Current userSockets length:', userSockets.size);
 
     socket.on('private message', ({ message, recipient }) => {
         console.log('Received private message:', { message, recipient });
         const recipientSocket = userSockets.get(recipient);
         if (recipientSocket) {
             console.log('Sending message to recipient:', recipient);
+            console.log('Recipient socket:', recipientSocket.id);
             recipientSocket.emit('private message', {
                 sender: socket.user,
                 message: message
@@ -99,13 +90,11 @@ io.use(authenticate).on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.user);
         userSockets.delete(socket.user);
-        console.log('Updated userSockets:', userSockets);
+        console.log('Updated userSockets:', userSockets.size);
     });
 });
 
-
 server.listen(port, () => {
-    console.log('The Chat Application is running on port', port)
-})
+    console.log('The Chat Application is running on port', port);
+});
